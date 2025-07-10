30dc497115b5e231c97671e0e3d9204628e5c19e
jwah101 <hon_achernar@naver.com>
2025-07-10 20:08:15 +0900

    0710 다크/라이트모드

--- 0710/8. 다크모드만들기/script.js
+++ 0710/8. 다크모드만들기/script.js
@@ -2,8 +2,13 @@ const darkmode = document.querySelector('.darkmode')
 const body = document.body;
 
 darkmode.addEventListener('click', function(){
-alert(darkmode.value)
-  
-body.style.background = 'gray';
-body.style.color = 'white';
+ if (darkmode.value == '다크모드'){
+  body.style.background = 'black';
+  body.style.color = 'white';
+  darkmode.value = '라이트모드'
+ } else if (darkmode.value == '라이트모드') {
+  body.style.background = 'white';
+  body.style.color = 'black';
+  darkmode.value ='다크모드';
+ }
 })
